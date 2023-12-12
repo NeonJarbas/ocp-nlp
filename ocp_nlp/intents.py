953c212fcbddbbf04d0e1b146f76f02676133bfe
JarbasAi <jarbasai@mailfence.com>
2023-12-12 20:10:54 +0000

    player state context
    DRY - subclass from OVOSAbstractApplication instead

--- ocp_nlp/intents.py
+++ ocp_nlp/intents.py
@@ -1,14 +1,13 @@
 import os
 import random
-import re
 from os.path import join, dirname
 from threading import RLock
 
-from ovos_bus_client.message import Message, dig_for_message
+from ovos_bus_client.message import Message
 from ovos_core.intent_services import IntentMatch
-from ovos_utils.enclosure.api import EnclosureAPI
 from ovos_utils.log import LOG
 from ovos_utils.messagebus import FakeBus
+from ovos_workshop.app import OVOSAbstractApplication
 from padacioso import IntentContainer
 
 from ocp_nlp.classify import BinaryPlaybackClassifier, BiasedMediaTypeClassifier, MediaTypeClassifier
@@ -16,77 +15,86 @@ from ocp_nlp.constants import OCP_ID, MediaType, PlaybackType, PlaybackMode, Pla
 from ocp_nlp.search import OCPQuery
 
 
-class OCPPipelineMatcher:
+class OCPPipelineMatcher(OVOSAbstractApplication):
 
     def __init__(self, bus=None, config=None, entities_path=f"{dirname(__file__)}/sparql_ocp"):
-        self.bus = bus or FakeBus()
+        super().__init__(skill_id=OCP_ID, bus=bus or FakeBus(),
+                         resources_dir=f"{dirname(__file__)}")
+
         self.entities_path = entities_path
         # TODO - auto download OCP dataset to a XDG directory
 
         self.ocp_clfs = {}
         self.m_clfs = {}
 
-        self._dialogs = {}  # lang: {name: [utts]}
-        self._intents = {}  # lang: {name: [utts]}
-        self._vocs = {}  # lang: {name: [utts]}
-
         self.config = config or {}
         self.search_lock = RLock()
-        self.player_state = PlayerState.STOPPED  # TODO - track state via bus
+        self.player_state = PlayerState.STOPPED
 
         self.pipeline_engines = {}
 
-        self.enclosure = EnclosureAPI(self.bus, skill_id=OCP_ID)
-
-        self.load_resource_files()
         self.register_ocp_api_events()
         self.register_ocp_intents()
 
     def load_resource_files(self):
-        # TODO - filter by native languages
-        langs = ["en-us"]
-        for lang in langs:
-            self._intents[lang] = {}
-            self._dialogs[lang] = {}
-            self._vocs[lang] = {}
+        intents = {}
+        for lang in self.native_langs:
+            intents[lang] = {}
             locale_folder = join(dirname(__file__), "locale", lang)
             for f in os.listdir(locale_folder):
                 path = join(locale_folder, f)
-                if f.endswith(".dialog"):
-                    with open(path) as intent:
-                        samples = intent.read().split("\n")
-                        for idx, s in enumerate(samples):
-                            samples[idx] = s.replace("{{", "{").replace("}}", "}")
-                        self._dialogs[lang][f] = samples
                 if f.endswith(".intent"):
                     with open(path) as intent:
                         samples = intent.read().split("\n")
                         for idx, s in enumerate(samples):
                             samples[idx] = s.replace("{{", "{").replace("}}", "}")
-                        self._intents[lang][f] = samples
-                if f.endswith(".voc"):
-                    with open(path) as intent:
-                        samples = intent.read().split("\n")
-                        self._vocs[lang][f] = samples
+                        intents[lang][f] = samples
+        return intents
 
     def register_ocp_api_events(self):
         """
         Register messagebus handlers for OCP events
         """
         self.bus.on("ovos.common_play.search", self.handle_search_query)
+        self.bus.on('ovos.common_play.player.state', self.handle_player_state_update)
 
     def register_ocp_intents(self):
+        intent_files = self.load_resource_files()
+
         intents = ["play.intent", "open.intent",
                    "next.intent", "prev.intent", "pause.intent",
                    "resume.intent"]
 
-        for lang, intent_data in self._intents.items():
+        for lang, intent_data in intent_files.items():
             self.pipeline_engines[lang] = IntentContainer()
             for intent_name in intents:
                 samples = intent_data.get(intent_name)
                 LOG.debug(f"registering OCP intent: {intent_name}")
                 self.pipeline_engines[lang].add_intent(
                     intent_name.replace(".intent", ""), samples)
+
+    def handle_player_state_update(self, message):
+        """
+        Handles 'ovos.common_play.player.state' messages with player state updates
+        @param message: Message providing new "state" data
+        """
+        state = message.data.get("state")
+        if state is None:
+            raise ValueError(f"Got state update message with no state: "
+                             f"{message}")
+        if isinstance(state, int):
+            state = PlayerState(state)
+        if not isinstance(state, PlayerState):
+            raise ValueError(f"Expected int or PlayerState, but got: {state}")
+        if state == self.player_state:
+            return
+        LOG.debug(f"PlayerState changed: {repr(state)}")
+        if state == PlayerState.PLAYING:
+            self.player_state = PlayerState.PLAYING
+        elif state == PlayerState.PAUSED:
+            self.player_state = PlayerState.PAUSED
+        elif state == PlayerState.STOPPED:
+            self.player_state = PlayerState.STOPPED
 
     def load_clf(self, lang):
         if lang not in self.ocp_clfs:
@@ -116,6 +124,12 @@ class OCPPipelineMatcher:
             return None
         if match["name"] == "play":
             return self._process_play_query(utterance, lang, match)
+
+        if self.player_state == PlayerState.STOPPED:
+            # next / previous / pause / resume not targeted
+            # at OCP if playback is not happening / paused
+            # TODO - handle resume for last_played query, eg, previous day
+            return None
 
         return IntentMatch(intent_service="OCP_intents",
                            intent_type=f'ocp:{match["name"]}',  # TODO intent event handler
@@ -166,7 +180,8 @@ class OCPPipelineMatcher:
 
     def _process_play_query(self, utterance, lang, match):
         # if media is currently paused, empty string means "resume playback"
-        if self._should_resume(utterance, lang):
+        if self.player_state == PlayerState.PAUSED and \
+                self._should_resume(utterance, lang):
             # self.bus.emit(Message('ovos.common_play.resume'))
             return IntentMatch(intent_service="OCP_intents",
                                intent_type=f"ocp:resume",  # TODO intent event handler
@@ -219,14 +234,14 @@ class OCPPipelineMatcher:
         if num:
             phrase += " " + num
 
-        self._process_play_query(phrase, lang)
+        self._process_play_query(phrase, lang, {"conf": 0.99})
 
     # intent handlers
     def _do_play(self, phrase, lang, results, media_type=MediaType.GENERIC):
         self.bus.emit(Message('ovos.common_play.reset'))
         LOG.debug(f"Playing {len(results)} results for: {phrase}")
         if not results:
-            self.speak_dialog("cant.play", lang=lang,
+            self.speak_dialog("cant.play",
                               data={"phrase": phrase,
                                     "media_type": media_type})
         else:
@@ -269,98 +284,6 @@ class OCPPipelineMatcher:
                     self.voc_match(phrase, "Play", lang=lang, exact=True):
                 return True
         return False
-
-    def voc_match(self, utt: str, voc_filename: str, lang: str, exact: bool = False):
-        """
-        Determine if the given utterance contains the vocabulary provided.
-
-        By default the method checks if the utterance contains the given vocab
-        thereby allowing the user to say things like "yes, please" and still
-        match against "Yes.voc" containing only "yes". An exact match can be
-        requested.
-
-        The method first checks in the current Skill's .voc files and secondly
-        in the "res/text" folder of mycroft-core. The result is cached to
-        avoid hitting the disk each time the method is called.
-
-        Args:
-            utt (str): Utterance to be tested
-            voc_filename (str): Name of vocabulary file (e.g. 'yes' for
-                                'res/text/en-us/yes.voc')
-            lang (str): Language code, defaults to self.lang
-            exact (bool): Whether the vocab must exactly match the utterance
-
-        Returns:
-            bool: True if the utterance has the given vocabulary it
-        """
-        match = False
-        if lang not in self._vocs:
-            return False
-
-        _vocs = self._vocs[lang].get(voc_filename) or \
-                self._vocs[lang].get(voc_filename + ".voc") or \
-                []
-
-        if utt and _vocs:
-            if exact:
-                # Check for exact match
-                match = any(i.strip() == utt
-                            for i in _vocs)
-            else:
-                # Check for matches against complete words
-                match = any([re.match(r'.*\b' + i + r'\b.*', utt)
-                             for i in _vocs])
-
-        return match
-
-    def remove_voc(self, utt: str, voc_filename: str, lang: str) -> str:
-        """
-        Removes any vocab match from the utterance.
-        @param utt: Utterance to evaluate
-        @param voc_filename: vocab resource to remove from utt
-        @param lang: Optional language associated with vocab and utterance
-        @return: string with vocab removed
-        """
-        if lang not in self._vocs:
-            return utt
-
-        _vocs = self._vocs[lang].get(voc_filename) or \
-                self._vocs[lang].get(voc_filename + ".voc") or \
-                []
-        if utt:
-            # Check for matches against complete words
-            for i in _vocs:
-                # Substitute only whole words matching the token
-                utt = re.sub(r'\b' + i + r"\b", "", utt)
-        return utt
-
-    def speak_dialog(self, dialog: str, data: dict, lang: str):
-        samples = self._dialogs.get("lang", {}).get(dialog) or \
-                  self._dialogs.get("lang", {}).get(dialog + ".dialog")
-        if not samples:
-            utt = dialog
-        else:
-            utt = random.choice(samples)
-        data = {"utterance": utt, "lang": lang,
-                "meta": {'dialog': dialog.replace(".dialog", ""),
-                         'data': data}}
-        # no dialog renderer, do a manual replace, accounting for whitespaces and double brackets
-        for k, v in data.items():
-            if isinstance(v, str):
-                utt = utt.replace("{{", "{"). \
-                    replace("}}", "}"). \
-                    replace("{ ", "{"). \
-                    replace(" }", "}"). \
-                    replace("{" + k + "}", v)
-        # grab message that triggered speech so we can keep context
-        message = dig_for_message()
-        m = message.forward("speak", data) if message \
-            else Message("speak", data)
-        m.context["skill_id"] = OCP_ID
-        self.bus.emit(m)
-
-    def get_response(self, dialog):
-        return None  # TODO port this method from workshop
 
     # search
     def _search(self, phrase, media_type, lang: str):
@@ -490,7 +413,6 @@ class OCPPipelineMatcher:
 
 
 if __name__ == "__main__":
-
     ocp = OCPPipelineMatcher()
     print(ocp.match_high("play metallica", "en-us"))
     # IntentMatch(intent_service='OCP_intents',
@@ -499,8 +421,6 @@ if __name__ == "__main__":
     #                'entities': {'album_name': 'Metallica', 'artist_name': 'Metallica'},
     #                'conf': 0.96, 'lang': 'en-us'},
     #   skill_id='ovos.common_play', utterance='play metallica')
-    print(ocp.match_high("put on some metallica", "en-us"))
-    # None
 
     print(ocp.match_medium("put on some metallica", "en-us"))
     # IntentMatch(intent_service='OCP_media',
@@ -510,8 +430,6 @@ if __name__ == "__main__":
     #                'query': 'put on some metallica',
     #                'conf': 0.9578441098114333},
     #   skill_id='ovos.common_play', utterance='put on some metallica')
-    print(ocp.match_medium("i wanna hear metallica", "en-us"))
-    # None
 
     print(ocp.match_fallback("i wanna hear metallica", "en-us"))
     #  IntentMatch(intent_service='OCP_fallback',
